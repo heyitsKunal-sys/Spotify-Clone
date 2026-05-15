de5bfc5d96ba9cbcb15c4b3a922ad5ad0badd042
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-05-15 21:09:13 +0530

    commit

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-console.log("kya bolti h public?");
 
-async function main(){
-    let a = await fetch(`./songs/${artist} /info.json`);
-    let response = await a.text();
-    console.log(response);
-    let element = document.createElement("div");
-    let div = document.createElement("div");
-    div.innerHTML = response;
-    element.appendChild(div);
-    document.body.appendChild(element);
+
+async function main() {
+    let a = await fetch("http://127.0.0.1:5500/songs/")
+    
 }
